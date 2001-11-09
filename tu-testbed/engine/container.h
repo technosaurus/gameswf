a1236bacb7e7be69cdcee76578456bc816fcf349
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2001-11-09 23:59:51 +0000

    array<T>, a less horrible replacement for STL vector<T>

--- tu-testbed/engine/container.h
+++ tu-testbed/engine/container.h
@@ -13,34 +13,51 @@
 #define CONTAINER_H
 
 
-namespace tu {
-
-	// Butt-simple, somewhat opaque interface to a list class that
-	// contains void* only.  User is responsible for memory management
-	// of the objects pointed to.
-	class	void_list {
-	public:
-		void_list();
-		~void_list();
-
-		void	insert( ... );
-		void_list*	next();
-
-		// For STL-style iteration.
-		void_list*	begin();
-		void_list*	end() { return 0; }
-		void_list*	operator++() { return next(); }
-
-	private:
-		elem*	head;
-	};
+#include <engine/utility.h>
 
 
-	template< class T >
-	class array {
-		
-	};
+// Resizable array.  Don't put anything in here that needs a
+// destructor called on it when the array is destructed.
+template<class T>
+class array {
+public:
+	array(int size_hint = 0) : m_buffer(0), m_size(0) { resize(size_hint); }
+	~array() { resize(0); }
 
+	T&	operator[](int index) { assert(index >= 0 && index < m_size); return m_buffer[index]; }
+
+	int	size() { return m_size; }
+	void	push_back(const T& val) { resize(m_size + 1); (*this)[m_size-1] = val; }
+
+	void	resize(int new_size)
+	// Preserve existing elements.  Newly created elements are unintialized.
+	// TODO: should not be inline!  No reason to bloat the code.
+	{
+		// TODO: does an allocation every time.  Should probably be a little smarter.
+		// Although maybe it's better to let realloc() work its magic.
+
+		assert(new_size >= 0);
+
+		m_size = new_size;
+		if (new_size == 0) {
+			if (m_buffer) {
+				free(m_buffer);
+			}
+			m_buffer = 0;
+		} else {
+			if (m_buffer) {
+				m_buffer = (T*) realloc(m_buffer, sizeof(T) * m_size);
+			} else {
+				m_buffer = (T*) malloc(sizeof(T) * m_size);
+			}
+		}			
+	}
+
+	// void	remove(int index);
+
+private:
+	T*	m_buffer;
+	int	m_size;
 };
 
 
