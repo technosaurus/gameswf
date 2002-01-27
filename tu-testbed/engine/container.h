812455b664f67467f8f4f29dfd50ae5ad949a5aa
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2002-01-27 23:44:26 +0000

    Fleshed out hash<> some more; not fixed size anymore, and ready for a string key drop-in.

--- tu-testbed/engine/container.h
+++ tu-testbed/engine/container.h
@@ -5,8 +5,10 @@
 
 // Generic C++ containers.  Problem: STL is murder on compile times,
 // and is hard to debug.  And also it's really easy to shoot yourself
-// in the foot.  These are not as comprehensive, but might be
-// better in those other dimensions.
+// in the foot.  In general I don't like it very much.
+//
+// These are not as comprehensive or general, but I think might be
+// more usable for day-to-day coding.
 
 
 #ifndef CONTAINER_H
@@ -19,11 +21,14 @@
 template<class T>
 class array {
 // Resizable array.  Don't put anything in here that needs a
-// destructor called on it when the array is destructed.
+// destructor called on it when the array is destructed.  Don't keep
+// the address of an element; the array contents may move around as it
+// gets resized.
 public:
-	array(int size_hint = 0) : m_buffer(0), m_size(0), m_buffer_size(0) { resize(size_hint); }
+	array() : m_buffer(0), m_size(0), m_buffer_size(0) {}
+	array(int size_hint) : m_buffer(0), m_size(0), m_buffer_size(0) { resize(size_hint); }
 	~array() { resize(0); }
-
+	
 	T&	operator[](int index) { assert(index >= 0 && index < m_size); return m_buffer[index]; }
 	const T&	operator[](int index) const { assert(index >= 0 && index < m_size); return m_buffer[index]; }
 
@@ -66,6 +71,15 @@ public:
 
 	// void	remove(int index);
 
+	void	copy_members(const array<T>& a)
+	// UNSAFE!  Low-level utility function: replace this array's
+	// members with a's members.
+	{
+		m_buffer = a.m_buffer;
+		m_size = a.m_size;
+		m_buffer_size = a.m_buffer_size;
+	}
+
 private:
 	T*	m_buffer;
 	int	m_size;
@@ -73,52 +87,157 @@ private:
 };
 
 
-inline int	compute_hash(const char* data, int size)
-// Computes a hash of the given buffer.
+template<class T>
+class fixed_size_hash
+// Computes a hash of an object's representation.
 {
-	int	h = 0;
-	while (size > 0) {
-		h = (h << 5) + *data * 101;	// hm, replace with a real hash function.
-		data++;
-		size--;
-	}
-	return h;
-}
-
-
-template<class T, class U>
-class hash {
-// Really stupid hash, for hashing using fixed-sized objects as keys.
 public:
-	hash() {}
+	static int	compute(const T& data)
+	{
+		unsigned char*	p = (unsigned char*) &data;
+		int	size = sizeof(T);
 
-	void	add(T key, U value) {
-		int	hash_value = compute_hash((char*) &key, sizeof(key));
+		int	h = 0;
+		while (size > 0) {
+			h = (h << 5) + *p * 101;	// hm, replace with a real hash function.
+			p++;
+			size--;
+		}
+		return h;
+	}
+};
+
+
+template<class T, class U, class hash_functor = fixed_size_hash<T> >
+class hash {
+// Fairly stupid hash table.
+//
+// Never shrinks, unless you explicitly clear() or resize() it.
+// Expands on demand, though.  For best results, if you know roughly
+// how big your table will be, default it to that size when you create
+// it.
+public:
+	hash() { m_entry_count = 0;  m_size_mask = 0; }
+	hash(int size_hint) { m_entry_count = 0; m_size_mask = 0; resize(size_hint); }
+
+	void	add(T key, U value)
+	// Add a new value to the hash table, under the specified key.
+	{
+		assert(get(key, NULL) == false);
+
+		m_entry_count++;
+		check_expand();
+
+		int	hash_value = hash_functor::compute(key);
 		entry	e;
 		e.key = key;
 		e.value = value;
 
-		int	index = hash_value & 4095;
+		int	index = hash_value & m_size_mask;	// % m_table.size();
 		m_table[index].push_back(e);
 	}
 
 	void	clear()
+	// Remove all entries from the hash table.
 	{
-		for (int i = 0; i < 4096; i++) {
+		for (int i = 0; i < m_table.size(); i++) {
 			m_table[i].clear();
 		}
+		m_table.clear();
+		m_entry_count = 0;
+		m_size_mask = 0;
 	}
 
-	bool	get(T key, U* value) {
-		int	hash_value = compute_hash((char*) &key, sizeof(key));
-		int	index = hash_value & 4095;
+
+	bool	get(T key, U* value)
+	// Retrieve the value under the given key.
+	//
+	// If there's no value under the key, then return false and leave
+	// *value alone.
+	//
+	// If there is a value, return true, and set *value to the entry's
+	// value.
+	//
+	// If value == NULL, return true or false according to the
+	// presence of the key, but don't touch *value.
+	{
+		if (m_table.size() == 0) {
+			return false;
+		}
+
+		int	hash_value = hash_functor::compute(key);
+		int	index = hash_value % m_table.size();
 		for (int i = 0; i < m_table[index].size(); i++) {
 			if (m_table[index][i].key == key) {
-				*value = m_table[index][i].value;
+				if (value) {
+					*value = m_table[index][i].value;
+				}
 				return true;
 			}
 		}
 		return false;
+	}
+
+	void	check_expand()
+	// Resize the hash table, if it looks like the size is too small
+	// for the current number of entries.
+	{
+		int	new_size = m_table.size();
+
+		if (m_table.size() == 0 && m_entry_count > 0) {
+			// Need to expand.  Let's make the base table size 256
+			// elements.
+			new_size = 256;
+		} else if (m_table.size() * 2 < m_entry_count) {
+			// Expand.
+			new_size = (m_entry_count * 3) >> 1;
+		}
+
+		if (new_size != m_table.size()) {
+			resize(new_size);
+		}
+	}
+
+
+	void	resize(int new_size)
+	// Resize the hash table to the given size (Rehash the contents of
+	// the current table).
+	{
+		if (new_size <= 0) {
+			// Special case.
+			clear();
+			return;
+		}
+
+		// Force new_size to be a power of two.
+		int	bits = fchop(log2(new_size-1) + 1);
+		assert((1 << bits) >= new_size);
+
+		new_size = 1 << bits;
+		m_size_mask = new_size - 1;
+
+		array< array<entry> >	new_table(new_size);
+		// Init entries in new_table, since constructors aren't
+		// called.
+		for (int i = 0; i < new_size; i++) {
+			new_table[i].copy_members(array<entry>(0));
+		}
+
+		// Copy all entries to the new table.
+		{for (int i = 0; i < m_table.size(); i++) {
+			for (int j = 0; j < m_table[i].size(); j++) {
+				entry&	e = m_table[i][j];
+				int	hash_value = hash_functor::compute(e.key);
+				
+				int	index = hash_value & m_size_mask;	// % new_table.size();
+				new_table[index].push_back(e);
+			}
+			m_table[i].clear();
+		}}
+		m_table.clear();
+
+		// Replace old table with new table.
+		m_table.copy_members(new_table);
 	}
 
 private:
@@ -126,7 +245,10 @@ private:
 		T	key;
 		U	value;
 	};
-	array<entry>	m_table[4096];
+	
+	int	m_entry_count;
+	int	m_size_mask;
+	array< array<entry> >	m_table;
 };
 
 
