15a29a1fa71ce603cc0ff6563a0cac51665277c0
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2002-05-24 03:11:41 +0000

    work-in-progress on adapting Dale Schumacher's image scaling code.

---
+++ tu-testbed/engine/image_filters.h
@@ -0,0 +1,38 @@
+// image_filters.h	-- Thatcher Ulrich <tu@tulrich.com> 2002
+
+// This source code has been donated to the Public Domain.  Do
+// whatever you want with it.
+
+// Image rescaling routines.
+
+
+#ifndef IMAGE_FILTERS_H
+#define IMAGE_FILTERS_H
+
+
+namespace image_filters
+{
+	enum filter_type {
+		BOX,
+		TRIANGLE,
+		BELL,
+		B_SPLINE,
+		SOME_RANDOM_CUBIC,
+		LANCZOS3,
+		MITCHELL	// This one is alleged to be pretty nice.
+	};
+
+	void	resample(SDL_Surface* out,
+			 SDL_Surface* in, float in_x0, float in_y0, float in_x1, float in_y1);
+};
+
+
+#endif // IMAGE_FILTERS_H
+
+// Local Variables:
+// mode: C++
+// c-basic-offset: 8 
+// tab-width: 8
+// indent-tabs-mode: t
+// End:
+
