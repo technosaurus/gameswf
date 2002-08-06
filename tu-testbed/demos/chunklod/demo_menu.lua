2bb4e7c01102af8007d4f29c9990968cee56c93f
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2002-08-06 04:18:04 +0000

    Some last-minute tweaks for the demo release.

--- tu-testbed/demos/chunklod/demo_menu.lua
+++ tu-testbed/demos/chunklod/demo_menu.lua
@@ -38,10 +38,10 @@ end
 -- main script; show a menu, run command, repeat
 while 1 do
 	write("\n")
-	write("1. maui\n")
-	write("2. riverblue\n")
-	write("3. puget\n")
-	write("4. crater\n")
+	write("1. crater\n")
+	write("2. maui\n")
+	write("3. riverblue\n")
+	write("4. puget\n")
 	write("5. hawaii\n")
 	write("6. kauai\n")
 	write("Q. quit\n")
@@ -52,14 +52,14 @@ while 1 do
 
 	if (choice == "q" or choice == "Q") then
 		exit(0)
-	elseif (choice == "1") then
-		demo("maui_2k.chu", "maui_4k_d5.tqt")
-	elseif (choice == "2") then
-		demo("riverblue_e1_d8.chu", "riverblue_16k_d7.tqt")
-	elseif (choice == "3") then
-		demo("puget_e4_d9.chu", "puget_32k_d9.tqt")
-	elseif (choice == "4") then
+	elseif (choice == "1" or choice == "") then
 		demo("crater/crater.chu", "crater/crater.jpg")
+	elseif (choice == "2") then
+		demo("maui_2k.chu", "maui_4k_d5.tqt")
+	elseif (choice == "3") then
+		demo("riverblue_e1_d8.chu", "riverblue_16k_d7.tqt")
+	elseif (choice == "4") then
+		demo("puget_e4_d9.chu", "puget_32k_d9.tqt")
 	elseif (choice == "5") then
 		demo("island_e1_d9.chu", "island_16k_d7.tqt")
 	elseif (choice == "6") then
