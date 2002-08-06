83cb538a1e2c8a115908ad940e25488c0d78e641
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2002-08-06 03:38:57 +0000

    Updated README and demo_menu.lua (demo_menu now prints a message if the expected datafiles don't exist)

--- trunk/tu-testbed/demos/chunklod/demo_menu.lua
+++ trunk/tu-testbed/demos/chunklod/demo_menu.lua
@@ -1,7 +1,41 @@
 -- launcher for presentation demos, requires only lua.exe
 
-options = " -f -w 1024 -h 768 -2"
+-- add -f to the string below to get fullscreen
+options = " -w 1024 -h 768 -2"
 
+
+function file_is_readable(file)
+-- function to test whether the given file can be read from.
+	local handle = openfile(file, "rb")
+	if handle == nil then
+		-- failure
+		return nil
+	else
+		-- success
+		closefile(handle)
+		return 1
+	end
+end
+
+
+function demo(chunkfile, texturefile)
+-- launch the chunkdemo program, using the given files as input.
+	if not file_is_readable(chunkfile) then
+		write("can't open " .. chunkfile .. " -- did you download the\n");
+		write("data?  See http://tulrich.com/geekstuff/chunklod.html\n");
+	elseif not file_is_readable(texturefile) then
+		write("can't open " .. texturefile .. " -- did you download the\n");
+		write("data?  See http://tulrich.com/geekstuff/chunklod.html\n");
+	else
+		-- files are available.
+		local cmd = "chunkdemo.exe " .. chunkfile .. " " .. texturefile .. options;
+		write("running " .. cmd .. "...\n");
+		execute(cmd);
+	end
+end
+
+
+-- main script; show a menu, run command, repeat
 while 1 do
 	write("\n")
 	write("1. maui\n")
@@ -19,16 +53,16 @@ while 1 do
 	if (choice == "q" or choice == "Q") then
 		exit(0)
 	elseif (choice == "1") then
-		execute("chunkdemo.exe maui_2k.chu maui_4k_d5.tqt " .. options)
+		demo("maui_2k.chu", "maui_4k_d5.tqt")
 	elseif (choice == "2") then
-		execute("chunkdemo.exe riverblue_e1_d8.chu riverblue_16k_d7.tqt " .. options)
+		demo("riverblue_e1_d8.chu", "riverblue_16k_d7.tqt")
 	elseif (choice == "3") then
-		execute("chunkdemo.exe puget_e4_d9.chu puget_32k_d9.tqt " .. options)
+		demo("puget_e4_d9.chu", "puget_32k_d9.tqt")
 	elseif (choice == "4") then
-		execute("chunkdemo.exe " .. options)
+		demo("crater/crater.chu", "crater/crater.jpg")
 	elseif (choice == "5") then
-		execute("chunkdemo.exe island_e1_d9.chu island_16k_d7.tqt " .. options)
+		demo("island_e1_d9.chu", "island_16k_d7.tqt")
 	elseif (choice == "6") then
-		execute("chunkdemo.exe kauai_e1_d9.chu kauai_8k_d6.tqt " .. options)
+		demo("kauai_e1_d9.chu", "kauai_8k_d6.tqt")
 	end
 end
