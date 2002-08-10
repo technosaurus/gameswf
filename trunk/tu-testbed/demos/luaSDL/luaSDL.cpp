4d8341ce879dd5874eeaa6cb6e5196ecbd93b956
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2002-08-10 05:41:28 +0000

    ported luaSDL.exe to use tolua instead of luaswig.
    No major problems.  tolua has a few nice touches over luaswig.

--- trunk/tu-testbed/demos/luaSDL/luaSDL.cpp
+++ trunk/tu-testbed/demos/luaSDL/luaSDL.cpp
@@ -3,64 +3,63 @@
 // This source code has been donated to the Public Domain.  Do
 // whatever you want with it.
 
-// Lua toplevel, bound to SDL.  (experimental console GUI in progress, off by default)
+// Lua toplevel, bound to SDL.
 
 
 #include <stdio.h>
 #include <string.h>
 
-#include <engine/config.h>
 extern "C" {
+#include "lua.h"
+#include "lualib.h"
 #include "luselib.h"	// TODO: move lua_uselibopen() to engine/config.cpp
 }
 
-//#define USE_GUI	1
-
-#ifdef USE_GUI
-#include "gui_console.h"
-#endif // USE_GUI
-
-
 // Init function to bind SDL to Lua.
-extern "C" int luaSDL_initialize(lua_State *L);
+extern "C" int tolua_SDL_open(lua_State *L);
+extern "C" void tolua_SDL_close(lua_State *L);
+
+
+static const int	LUA_STACK_SIZE = 2048;
 
 
 int	main(int argc, char *argv[])
 {
-	config::open();
-	lua_uselibopen(config::L);
+	// Init Lua.
+	lua_State*	L = lua_open(LUA_STACK_SIZE);
 
-	luaSDL_initialize(config::L);
+	// Init the standard Lua libs.
+	lua_baselibopen(L);
+	lua_iolibopen(L);
+	lua_strlibopen(L);
+	lua_mathlibopen(L);
+
+//	lua_uselibopen(config::L);
+
+//	luaSDL_initialize(config::L);
+	tolua_SDL_open(L);
 
 	// Load any files specified on the command-line.
 	for (int i = 1; i < argc; i++) {
-		lua_dofile(config::L, argv[i]);
+		lua_dofile(L, argv[i]);
 	}
 
 	if (argc <= 1) {
-
-#ifdef USE_GUI
-
-		gui_console();
-
-#else // not USE_GUI
-
 		// Interpreter loop.
 		for (;;) {
 			// TODO: update this with lua.exe -like support for backslash input extending, etc.
 
 			// Get a command line.
 			char	buffer[1000];
-			printf( "> " );
-			fgets( buffer, 1000, stdin );
+			printf("> ");
+			fgets(buffer, 1000, stdin);
 
 			// Execute it.
-			lua_dostring( config::L, buffer );
+			lua_dostring(L, buffer);
 		}
-
-#endif // not USE_GUI
-
 	}
+
+	tolua_SDL_close(L);
 
 	return 0;
 }
