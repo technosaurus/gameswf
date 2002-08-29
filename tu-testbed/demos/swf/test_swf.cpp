c4a6fc167275c3504e52cd55ea67823284430730
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2002-08-29 15:16:46 +0000

    Test program for swf

---
+++ tu-testbed/demos/swf/test_swf.cpp
@@ -0,0 +1,16 @@
+// test_swf.cpp	-- Thatcher Ulrich <tu@tulrich.com> 2002
+
+// This source code has been donated to the Public Domain.  Do
+// whatever you want with it.
+
+// Test program for swf parser.
+
+
+#include "swf.h"
+
+
+int	main(int argc, char *argv[])
+{
+	return 0;
+}
+
