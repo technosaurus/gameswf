2681e61ddf290866e5c5f2e2439b97ff5f4b19e4
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2002-12-20 03:57:08 +0000

    buncha tweaks to actions & play states

--- tu-testbed/demos/swf/swf_impl.cpp
+++ tu-testbed/demos/swf/swf_impl.cpp
@@ -130,12 +130,14 @@ namespace swf
 		rgba	m_color;
 		matrix	m_gradient_matrix;
 		array<gradient_record>	m_gradients;
+		swf::render::bitmap_info*	m_gradient_bitmap_info;
 		bitmap_character*	m_bitmap_character;
 		matrix	m_bitmap_matrix;
 
 		fill_style()
 			:
 			m_type(0),
+			m_gradient_bitmap_info(0),
 			m_bitmap_character(0)
 		{
 			assert(m_gradients.size() == 0);
@@ -164,7 +166,12 @@ namespace swf
 				// 0x10: linear gradient fill
 				// 0x12: radial gradient fill
 
-				m_gradient_matrix.read(in);
+				matrix	input_matrix;
+				input_matrix.read(in);
+
+				m_gradient_matrix.set_identity();
+				m_gradient_matrix.concatenate_scale(1.0f / 256.0f);
+				m_gradient_matrix.concatenate(input_matrix);
 				
 				// GRADIENT
 				int	num_gradients = in->read_u8();
@@ -204,6 +211,59 @@ namespace swf
 			}
 		}
 
+
+		rgba	sample_gradient(int ratio) const
+		// Return the color at the specified ratio into our gradient.
+		// Ratio is in [0, 255].
+		{
+			assert(ratio >= 0 && ratio <= 255);
+			assert(m_type == 0x10 || m_type == 0x12);
+			assert(m_gradients.size() > 0);
+
+			for (int i = 0; i < m_gradients.size(); i++)
+			{
+				if (m_gradients[i].m_ratio >= ratio)
+				{
+					if (i == 0)
+					{
+						return m_gradients[i].m_color;
+					}
+					float	f = (ratio - m_gradients[i - 1].m_ratio) / float(m_gradients[i].m_ratio - m_gradients[i - 1].m_ratio);
+
+					rgba	result;
+					result.set_lerp(m_gradients[i - 1].m_color, m_gradients[i].m_color, f);
+					return result;
+				}
+			}
+			return m_gradients.back().m_color;
+		}
+
+		swf::render::bitmap_info*	create_gradient_bitmap() const
+		// Make a bitmap_info* corresponding to our gradient.
+		// We can use this to set the gradient fill style.
+		{
+			assert(m_type == 0x10);	// linear only, for the moment.  TODO radial fill.
+
+			if (m_type == 0x10)
+			{
+				image::rgba*	im = image::create_rgba(256, 1);
+
+				for (int i = 0; i < im->m_width; i++)
+				{
+					rgba	sample = sample_gradient(i);
+					im->set_pixel(i, 0, sample.m_r, sample.m_g, sample.m_b, sample.m_a);
+				}
+
+				swf::render::bitmap_info*	bi = swf::render::create_bitmap_info(im);
+				delete im;
+
+				return bi;
+			}
+
+			return NULL;
+		}
+
+
 		void	apply(int fill_side) const
 		// Push our style parameters into the renderer.
 		{
@@ -217,8 +277,20 @@ namespace swf
 				// 0x10: linear gradient fill
 				// 0x12: radial gradient fill
 
-				// Hack.
-				swf::render::fill_style_color(fill_side, m_color);
+				if (m_type == 0x10 && m_gradient_bitmap_info == NULL)
+				{
+					(const_cast<fill_style*>(this))->m_gradient_bitmap_info = create_gradient_bitmap();
+				}
+
+				if (m_gradient_bitmap_info)
+				{
+					swf::render::fill_style_bitmap(fill_side, m_gradient_bitmap_info, m_gradient_matrix, swf::render::WRAP_CLAMP);
+				}
+				else
+				{
+					// Hack.
+					swf::render::fill_style_color(fill_side, m_color);
+				}
 			}
 			else if (m_type == 0x40
 				 || m_type == 0x41)
@@ -840,20 +912,41 @@ namespace swf
 
 				if (next_frame < m_frame_count)
 				{
-					goto_frame(next_frame);	// actions happen in here...
+					if (m_current_frame >= 0
+					    && m_current_frame < m_frame_count
+					    && next_frame > m_current_frame)
+					{
+						array<execute_tag*>&	playlist = m_playlist[m_current_frame];
+						for (int i = 0; i < playlist.size(); i++)
+						{
+							execute_tag*	e = playlist[i];
+							e->execute(this);
+						}
+					}
+					m_current_frame = next_frame;
 				}
+
+				do_actions();
 			}
+		}
+
+
+		void	do_actions()
+		// Take care of this frame's actions.
+		{
+			execute_actions(this, m_action_list);
+			m_action_list.resize(0);
 		}
 
 
 		void	goto_frame(int target_frame_number)
 		// Set the movie state at the specified frame number.
-		// If the target frame number is in the future, we can
-		// do this incrementally; otherwise we have to rewind
-		// and advance from the beginning.
 		{
 			IF_DEBUG(printf("goto_frame(%d)\n", target_frame_number));//xxxxx
 
+			m_current_frame = target_frame_number;
+			m_time_remainder = 0 /* @@ frame time */;
+#if 0
 			if (target_frame_number < m_current_frame)
 			{
 				// Can't incrementally update, so start from the beginning.
@@ -889,6 +982,7 @@ namespace swf
 			m_action_list.resize(0);
 
 //			IF_DEBUG(printf("m_time = %f, fr = %d\n", m_time, m_current_frame));
+#endif // 0
 		}
 
 		void	display()
@@ -2527,19 +2621,21 @@ namespace swf
 	struct sprite_instance : public character
 	{
 		sprite_definition*	m_def;
-		int	m_current_frame;
 		array<display_object_info>	m_display_list;	// active characters, ordered by depth.
 		array<action_buffer*>	m_action_list;
 
-		float	m_time;
+		play_state	m_play_state;
+		int	m_current_frame;
+		float	m_time_remainder;
 
 		virtual ~sprite_instance() {}
 
 		sprite_instance(sprite_definition* def)
 			:
 			m_def(def),
+			m_play_state(PLAY),
 			m_current_frame(-1),
-			m_time(0)
+			m_time_remainder(0)
 		{
 			assert(m_def);
 			m_id = def->m_id;
@@ -2552,12 +2648,23 @@ namespace swf
 
 		int	get_current_frame() const { return m_current_frame; }
 
+		void	set_play_state(play_state s)
+		// Stop or play the movie.
+		{
+			if (m_play_state != s)
+			{
+				m_time_remainder = 0;
+			}
+
+			m_play_state = s;
+		}
+
 		void	restart()
 		{
 			m_display_list.resize(0);
 			m_action_list.resize(0);
 			m_current_frame = -1;
-			m_time = 0;
+			m_time_remainder = 0;
 		}
 
 
@@ -2565,6 +2672,49 @@ namespace swf
 		{
 			assert(m_def && m_def->m_movie);
 
+			m_time_remainder += delta_time;
+			const float	frame_time = 1.0f / m_def->m_movie->m_frame_rate;
+
+			while (m_time_remainder >= frame_time)
+			{
+				m_time_remainder -= frame_time;
+			
+				float	dt = frame_time;
+				int	next_frame = m_current_frame + 1;
+				if (m_play_state == STOP)
+				{
+					dt = 0.0f;
+					next_frame = m_current_frame;
+				}
+
+				// Advance everything in the display list.
+				for (int i = 0; i < m_display_list.size(); i++)
+				{
+					matrix	sub_matrix = mat;
+					sub_matrix.concatenate(m_display_list[i].m_matrix);
+					m_display_list[i].m_character->advance(dt, m, sub_matrix);
+				}
+
+				if (next_frame < m_def->m_frame_count)
+				{
+					if (m_current_frame >= 0
+					    && m_current_frame < m_def->m_frame_count
+					    && next_frame > m_current_frame)
+					{
+						array<execute_tag*>&	playlist = m_def->m_playlist[m_current_frame];
+						for (int i = 0; i < playlist.size(); i++)
+						{
+							execute_tag*	e = playlist[i];
+							e->execute(this);
+						}
+					}
+					m_current_frame = next_frame;
+				}
+
+				do_actions();
+			}
+
+#if 0
 			// Call advance() on each object in our
 			// display list.  Takes care of sub-sprites & buttons.
 			for (int i = 0; i < m_display_list.size(); i++)
@@ -2589,15 +2739,25 @@ namespace swf
 			}
 
 			goto_frame(target_frame_number);
+#endif // 0
+		}
+
+		void	do_actions()
+		// Take care of this frame's actions.
+		{
+			execute_actions(this, m_action_list);
+			m_action_list.resize(0);
 		}
 
 
 		void	goto_frame(int target_frame_number)
-		// Set the movie state at the specified frame number.
-		// If the target frame number is in the future, we can
-		// do this incrementally; otherwise we have to rewind
-		// and advance from the beginning.
+		// Set the sprite state at the specified frame number.
 		{
+			IF_DEBUG(printf("goto_frame(%d)\n", target_frame_number));//xxxxx
+
+			m_current_frame = target_frame_number;
+			m_time_remainder = 0 /* @@ frame time */;
+#if 0
 			if (target_frame_number < m_current_frame)
 			{
 				// Can't incrementally update, so start from the beginning.
@@ -2630,6 +2790,7 @@ namespace swf
 			}
 
 //			IF_DEBUG(printf("m_time = %f, fr = %d\n", m_time, m_current_frame));
+#endif // 0
 		}
 
 		void	display()
@@ -3245,8 +3406,6 @@ namespace swf
 				switch (action_id)
 				{
 				default:
-				case 0x08:	// toggle quality
-				case 0x09:	// stop sounds
 					break;
 
 				case 0x00:	// end of actions.
@@ -3261,11 +3420,47 @@ namespace swf
 					break;
 
 				case 0x06:	// action play
-//					m->set_playing(true);
+					m->set_play_state(movie::PLAY);
 					break;
 
 				case 0x07:	// action stop
 					m->set_play_state(movie::STOP);
+					break;
+
+				case 0x08:	// toggle quality
+				case 0x09:	// stop sounds
+				case 0x0A:	// add
+				case 0x0B:	// subtract
+				case 0x0C:	// multiply
+				case 0x0D:	// divide
+				case 0x0E:	// equal
+				case 0x0F:	// less than
+				case 0x10:	// logical and
+				case 0x11:	// logical or
+				case 0x12:	// logical not
+				case 0x13:	// string equal
+				case 0x14:	// string length
+				case 0x15:	// substring
+				case 0x18:	// int
+				case 0x1C:	// eval
+				case 0x1D:	// set variable
+				case 0x20:	// set target expression
+				case 0x21:	// string concat
+				case 0x22:	// get property
+				case 0x23:	// set property
+				case 0x24:	// duplicate clip (sprite?)
+				case 0x25:	// remove clip
+				case 0x26:	// trace
+				case 0x27:	// start drag movie
+				case 0x28:	// stop drag movie
+				case 0x29:	// string less than
+				case 0x30:	// random
+				case 0x31:	// mb length
+				case 0x32:	// ord
+				case 0x33:	// chr
+				case 0x34:	// get timer
+				case 0x35:	// mb substring
+				case 0x37:	// mb chr
 					break;
 				}
 				pc++;	// advance to next action.
@@ -3295,13 +3490,18 @@ namespace swf
 				case 0x83:	// get url
 				{
 					// @@ TODO should call back into client app, probably...
+
+					// Print as text.
+					IF_DEBUG({
+						for (int i = 0; i < length; i++) { printf("%c", m_buffer[pc + 3 + i]); } printf("\n");
+					});
+
 					break;
 				}
 
 				case 0x8A:	// wait for frame
 				{
-					// @@ TODO I think this has to deal with incremental loading;
-					// i.e. we don't really care about it.
+					// @@ TODO I think this has to deal with incremental loading
 					break;
 				}
 
@@ -3324,6 +3524,15 @@ namespace swf
 					// m->goto_labeled_frame(frame_label);
 					break;
 				}
+
+				case 0x8D:	// wait for frame expression (?)
+				case 0x96:	// push data
+				case 0x99:	// branch always (goto)
+				case 0x9A:	// get url 2
+				case 0x9D:	// branch if true
+				case 0x9E:	// call frame
+				case 0x9F:	// goto expression (?)
+					break;
 				
 				}
 				pc = next_pc;
