f12c90620ec93fee33a5644a3e5553da31b4fbec
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2003-06-17 20:30:49 +0000

    checkpoint: some kd-tree code in progress

---
+++ trunk/tu-testbed/demos/test_kd_tree/test_kd_tree.cpp
@@ -0,0 +1,28 @@
+// test_kd_tree.cpp	-- Thatcher Ulrich <http://tulrich.com> 2003
+
+// This source code has been donated to the Public Domain.  Do
+// whatever you want with it.
+
+// Test program for kd-tree mesh collision.
+
+
+#include "engine/kd_tree_packed.h"
+#include "engine/kd_tree_dynamic.h"
+#include "engine/axial_box.h"
+
+
+int main(int argc, const char** argv)
+{
+
+	return 0;
+}
+
+
+
+// Local Variables:
+// mode: C++
+// c-basic-offset: 8 
+// tab-width: 8
+// indent-tabs-mode: t
+// End:
+
--- trunk/tu-testbed/engine/kd_tree_packed.cpp
+++ trunk/tu-testbed/engine/kd_tree_packed.cpp
@@ -9,21 +9,48 @@
 
 #include "engine/kd_tree_packed.h"
 
+#include "engine/axial_box.h"
+
+
+struct ray_test_result;
+struct ray_test_query;
+
 
 struct kd_tree_packed::node_chunk
 {
-	void	ray_test(ray_test_result* result, const ray_test_query& query, int node_idx = 0) const;
+	void	ray_test(
+		ray_test_result* result,
+		const ray_test_query& query,
+		const axial_box& node_bound,
+		int node_idx = 0) const;
 
 	void	get_child(const node_chunk** child, int* child_idx, int idx, int which_child) const;
 
 private:
+	struct leaf;
+
 	// Kudos to Christer Ericson for the sweet packed node union.
 	union node
 	{
 		float	m_plane;	// axis is packed into low 2 bits.  01==x, 02==y, 03==z
 		int	m_leaf_offset;	// if low 2 bits == 00 then this is an offset into the leaf array
 
-		// @@ can unions have member functions?
+		// @@ can unions have member functions?  MSVC says yes...
+
+		bool	is_leaf() const { return (m_leaf_offset & 3) == 00; }
+		const leaf*	get_leaf() const { return (const leaf*) (((const char*) this) + m_leaf_offset); }
+
+		int	get_axis() const
+		{
+			assert(is_leaf() == false);
+			return (m_leaf_offset & 3) - 1;
+		}
+
+		float	get_plane_offset() const
+		{
+			// @@ should we mask off bottom bits?  Or leave them alone?
+			return m_plane;
+		}
 	};
 
 	node	m_nodes[13];		// interior nodes
@@ -36,37 +63,49 @@ void	kd_tree_packed::node_chunk::ray_test(
 	ray_test_result* result,
 	const ray_test_query& query,
 	const axial_box& node_bound,
-	int node_idx = 0) const
+	int node_idx /* = 0 */) const
 // Ray test against the node_idx'th node in this chunk.
 {
-	assert(child >= 0 && child < 13);
+	assert(node_idx >= 0 && node_idx < 13);
 
-	const node&	n = m_node[node_idx];
+	const node&	n = m_nodes[node_idx];
 	if (n.is_leaf())
 	{
-		n.get_leaf()->ray_test(result, query);
+//x		n.get_leaf()->ray_test(result, query);
 	}
 	else
 	{
-		int	axis = node.get_axis();
-		float	plane_offset = node.get_plane_offset();
+		int	axis = n.get_axis();
+		float	plane_offset = n.get_plane_offset();
 
 		const node_chunk*	chunk;
 		int	child_idx;
 
 		// crossing node always gets queried.
 		get_child(&chunk, &child_idx, node_idx, 0);
-		chunk->ray_test(result, query, child_idx);
+		chunk->ray_test(result, query, node_bound, child_idx);
 
 		// cut the ray at plane_offset
 
+		axial_box	child_box(node_bound);
+
 		// if query reaches front child
+		child_box.set_axis_min(axis, plane_offset);
 		get_child(&chunk, &child_idx, node_idx, 1);
-		chunk->ray_test(result, query, child_idx);
+		chunk->ray_test(result, query, child_box, child_idx);
 		
 		// if query reaches back child
+		child_box = node_bound;
+		child_box.set_axis_max(axis, plane_offset);
 		get_child(&chunk, &child_idx, node_idx, 2);
-		chunk->ray_test(result, query, child_idx);
+		chunk->ray_test(result, query, child_box, child_idx);
 	}
 }
 
+
+// Local Variables:
+// mode: C++
+// c-basic-offset: 8 
+// tab-width: 8
+// indent-tabs-mode: t
+// End:
