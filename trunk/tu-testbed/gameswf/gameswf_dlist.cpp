3c1bcc2f80022289d1ea538ace494613b6101abc
castano <castano@4a52ceea-2c23-0410-a973-b1084b30d411>
2003-06-24 23:53:58 +0000

    New display list object.

---
+++ trunk/tu-testbed/gameswf/gameswf_dlist.cpp
@@ -0,0 +1,387 @@
+// gameswf_dlist.h	-- Thatcher Ulrich <tu@tulrich.com> 2003
+
+// This source code has been donated to the Public Domain.  Do
+// whatever you want with it.
+
+// A list of active characters.
+
+
+
+#include "gameswf_dlist.h"
+#include "gameswf_log.h"
+
+
+
+namespace gameswf
+{
+
+	int	display_list::find_display_index(int depth)
+	// Find the index in the display list matching the given
+	// depth.  Failing that, return the index of the first object
+	// with a larger depth.
+	{
+		int	size = m_display_object_array.size();
+		if (size == 0)
+		{
+			return 0;
+		}
+
+		// Binary search.
+		int	jump = size >> 1;
+		int	index = jump;
+		for (;;)
+		{
+			jump >>= 1;
+			if (jump < 1) jump = 1;
+
+			if (depth > m_display_object_array[index].m_depth) {
+				if (index == size - 1)
+				{
+					index = size;
+					break;
+				}
+				index += jump;
+			}
+			else if (depth < m_display_object_array[index].m_depth)
+			{
+				if (index == 0
+				    || depth > m_display_object_array[index - 1].m_depth)
+				{
+					break;
+				}
+				index -= jump;
+			}
+			else
+			{
+				// match -- return this index.
+				break;
+			}
+		}
+
+		assert(index >= 0 && index <= size);
+
+		return index;
+	}
+
+
+	int	display_list::get_display_index(int depth)
+	// Like the above, but looks for an exact match, and returns -1 if failed.
+	{
+		int	index = find_display_index(depth);
+		if (index >= m_display_object_array.size()
+			|| get_display_object(index).m_depth != depth)
+		{
+			// No object at that depth.
+			return -1;
+		}
+		return index;
+	}
+	
+
+	void	display_list::add_display_object(
+		movie* root_movie,
+		character* ch, 
+		Uint16 depth, 
+		const cxform& color_xform, 
+		const matrix& mat, 
+		float ratio)
+	{
+		assert(ch);
+
+		// Try to move an existing character before creating a new one.
+		int index = find_display_index(depth);
+
+		if (index < m_display_object_array.size()
+			&& get_display_object(index).m_depth == depth
+			&& get_display_object(index).m_character->get_id() == ch->get_id())
+		{
+			display_object_info&	di = m_display_object_array[index];
+			di.m_ref = true;
+			di.m_color_transform = color_xform;
+			di.m_matrix = mat;
+			di.m_ratio = ratio;
+			return;
+		}
+
+		// If the character needs per-instance state, then
+		// create the instance here, and substitute it for the
+		// definition.
+		if (ch->is_definition())
+		{
+			ch = ch->create_instance();
+			ch->restart();
+		}
+
+		display_object_info	di;
+		di.m_movie = root_movie;
+		di.m_ref = true;
+		di.m_character = ch;
+		di.m_depth = depth;
+		di.m_color_transform = color_xform;
+		di.m_matrix = mat;
+		di.m_ratio = ratio;
+
+		// Insert into the display list...
+		index = find_display_index(di.m_depth);
+
+		m_display_object_array.insert(index, di);
+	}
+
+
+	void	display_list::move_display_object(Uint16 depth, bool use_cxform, const cxform& color_xform, bool use_matrix, const matrix& mat, float ratio)
+	// Updates the transform properties of the object at
+	// the specified depth.
+	{
+		int	size = m_display_object_array.size();
+		if (size <= 0)
+		{
+			// error.
+			log_error("error: move_display_object() -- no objects on display list\n");
+//			assert(0);
+			return;
+		}
+
+		int	index = find_display_index(depth);
+		if (index < 0 || index >= size)
+		{
+			// error.
+			log_error("error: move_display_object() -- can't find object at depth %d\n", depth);
+//			assert(0);
+			return;
+		}
+
+		display_object_info&	di = m_display_object_array[index];
+		if (di.m_depth != depth)
+		{
+			// error
+			log_error("error: move_display_object() -- no object at depth %d\n", depth);
+//			assert(0);
+			return;
+		}
+
+		if (use_cxform)
+		{
+			di.m_color_transform = color_xform;
+		}
+		if (use_matrix)
+		{
+			di.m_matrix = mat;
+		}
+		di.m_ratio = ratio;
+	}
+
+
+	void	display_list::replace_display_object(character* ch, Uint16 depth, bool use_cxform, const cxform& color_xform, bool use_matrix, const matrix& mat, float ratio)
+	// Puts a new character at the specified depth, replacing any
+	// existing character.  If use_cxform or use_matrix are false,
+	// then keep those respective properties from the existing
+	// character.
+	{
+		int	size = m_display_object_array.size();
+		if (size <= 0)
+		{
+			// error.
+			assert(0);
+			return;
+		}
+
+		int	index = find_display_index(depth);
+		if (index < 0 || index >= size)
+		{
+			// error.
+			assert(0);
+			return;
+		}
+
+		display_object_info&	di = m_display_object_array[index];
+		if (di.m_depth != depth)
+		{
+			// error
+			IF_DEBUG(log_msg("warning: replace_display_object() -- no object at depth %d\n", depth));
+//			assert(0);
+			return;
+		}
+
+		// If the old character is an instance, then delete it.
+		if (di.m_character->is_instance())
+		{
+			delete di.m_character;
+			di.m_character = 0;
+		}
+
+		// Put the new character in its place.
+		assert(ch);
+
+		// If the character needs per-instance state, then
+		// create the instance here, and substitute it for the
+		// definition.
+		if (ch->is_definition())
+		{
+			ch = ch->create_instance();
+			ch->restart();
+		}
+
+		// Set the display properties.
+		di.m_ref = true;
+		di.m_character = ch;
+		if (use_cxform)
+		{
+			di.m_color_transform = color_xform;
+		}
+		if (use_matrix)
+		{
+			di.m_matrix = mat;
+		}
+		di.m_ratio = ratio;
+	}
+
+
+	void	display_list::remove_display_object(Uint16 depth)
+	// Removes the object at the specified depth.
+	{
+		int	size = m_display_object_array.size();
+		if (size <= 0)
+		{
+			// error.
+			log_error("remove_display_object: no characters in display list\n");
+			return;
+		}
+
+		int	index = find_display_index(depth);
+		if (index < 0 || index >= size)
+		{
+			// error -- no character at the given depth.
+			log_error("remove_display_object: no character at depth %d\n", depth);
+			return;
+		}
+
+		// Removing the character at get_display_object(index).
+		display_object_info&	di = m_display_object_array[index];
+
+		// Remove reference only.
+		di.m_ref = false;
+	}
+
+
+	void	display_list::clear()
+	// clear the display list.
+	{
+		int i, n = m_display_object_array.size();
+		for (i = 0; i < n; i++)
+		{
+			character*	ch = m_display_object_array[i].m_character;
+			if (ch->is_instance())
+			{
+				delete ch;
+			}
+		}
+
+		m_display_object_array.clear();
+	}
+
+
+	void	display_list::reset()
+	// reset the references to the display list.
+	{
+		//printf("### reset the display list!\n");
+		int i, n = m_display_object_array.size();
+		for (i = 0; i < n; i++)
+		{
+			m_display_object_array[i].m_ref = false;
+		}
+	}
+
+
+	void	display_list::update()
+	// remove unreferenced objects.
+	{
+		//printf("### update the display list!\n");
+
+		int r = 0;
+		int i, n = m_display_object_array.size();
+		for (i = n-1; i >= 0; i--)
+		{
+			display_object_info & dobj = m_display_object_array[i];
+
+			if (dobj.m_ref == false)
+			{
+				if (dobj.m_character->is_instance())
+				{
+					delete dobj.m_character;
+					dobj.m_character = NULL;
+				}
+				m_display_object_array.remove(i);
+				r++;
+			}
+		}
+
+		//printf("### removed characters: %4d active characters: %4d\n", r, m_display_object_array.size());
+	}
+
+
+	void	display_list::advance(float delta_time, movie* m)
+	// advance referenced characters.
+	{
+		int i, n = m_display_object_array.size();
+		for (i = 0; i < n; i++)
+		{
+			display_object_info & dobj = m_display_object_array[i];
+
+			if (dobj.m_ref == true)
+			{
+				dobj.m_character->advance(delta_time, m, dobj.m_matrix);
+			}
+		}
+	}
+
+	
+	void	display_list::advance(float delta_time, movie* m, const matrix& mat)
+	// advance referenced characters.
+	{
+		int i, n = m_display_object_array.size();
+		for (i = 0; i < n; i++)
+		{
+			display_object_info & dobj = m_display_object_array[i];
+
+			if (dobj.m_ref == true)
+			{
+				matrix	sub_matrix = mat;
+				sub_matrix.concatenate(dobj.m_matrix);
+				dobj.m_character->advance(delta_time, m, sub_matrix);
+			}
+		}
+	}
+
+
+	void	display_list::display(int m_total_display_count)
+	// Display the referenced characters. Lower depths
+	// are obscured by higher depths.
+	{
+		for (int i = 0; i < m_display_object_array.size(); i++)
+		{
+			display_object_info&	dobj = m_display_object_array[i];
+			dobj.m_display_number = m_total_display_count;
+			dobj.m_character->display(dobj);
+
+		//	printf("display %s\n", typeid(*(di.m_character)).name());
+		}
+	}
+
+
+	void	display_list::display(const display_info & di)
+	// Display the referenced characters.
+	{
+		for (int i = 0; i < m_display_object_array.size(); i++)
+		{
+			display_object_info&	dobj = m_display_object_array[i];
+
+			display_info	sub_di = di;
+			sub_di.concatenate(dobj);
+			dobj.m_character->display(sub_di);
+		}
+	}
+
+
+}
+
+
