9bbff94f3aeeaeff01f9e17bab61eebd91a62343
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2004-01-12 00:26:09 +0000

    weak_ptr<> fixes; fix gameswf bug in Key object listener -- use weak_ptr<> to keep track of listeners

--- tu-testbed/base/smart_ptr.h
+++ tu-testbed/base/smart_ptr.h
@@ -24,8 +24,9 @@
 // strong pointer temporarily while they are actively using the
 // object, to prevent the object from being deleted.
 template<class T>
-struct smart_ptr
+class smart_ptr
 {
+public:
 	smart_ptr(T* ptr)
 		:
 		m_ptr(ptr)
@@ -92,8 +93,9 @@ private:
 
 
 // Helper for making objects that can have weak_ptr's.
-struct weak_proxy
+class weak_proxy
 {
+public:
 	weak_proxy()
 		:
 		m_ref_count(0),
@@ -104,8 +106,8 @@ struct weak_proxy
 	// weak_ptr's call this to determine if their pointer is valid or not.
 	bool	is_alive() const { return m_alive; }
 
-	// Only the actual object is allowed to call this.
-	void	notify_object_died(bool alive) { m_alive = alive; }
+	// Only the actual object should call this.
+	void	notify_object_died() { m_alive = false; }
 
 	void	add_ref() { assert(m_ref_count >= 0); m_ref_count++; }
 	void	drop_ref()
@@ -142,8 +144,9 @@ private:
 // if (smart_ptr<my_type> ptr = m_weak_ptr_to_my_type) { ... use ptr->whatever() safely in here ... }
 
 template<class T>
-struct weak_ptr
+class weak_ptr
 {
+public:
 	weak_ptr()
 		:
 		m_ptr(0)
@@ -182,7 +185,18 @@ struct weak_ptr
 	void	operator=(const smart_ptr<T>& ptr) { operator=(ptr.get_ptr()); }
 
 	// Conversion to smart_ptr.
-	operator smart_ptr<T>() const
+	operator smart_ptr<T>()
+	{
+		check_proxy();
+		return smart_ptr<T>(m_ptr);
+	}
+
+	bool	operator==(T* ptr) { check_proxy(); return m_ptr == ptr; }
+	bool	operator==(const smart_ptr<T>& ptr) { check_proxy(); return m_ptr == ptr.get_ptr(); }
+
+private:
+	void check_proxy()
+	// Set m_ptr to NULL if the object died.
 	{
 		if (m_ptr)
 		{
@@ -194,10 +208,8 @@ struct weak_ptr
 				m_ptr = NULL;
 			}
 		}
-		return smart_ptr<T>(m_ptr);
 	}
 
-private:
 	smart_ptr<weak_proxy>	m_proxy;
 	T*	m_ptr;
 };
@@ -208,22 +220,35 @@ private:
 #if 0
 ref_counted
 {
-	int	m_ref_count
+	mutable int	m_ref_count
+	mutable weak_proxy* m_weak_proxy;
 
-	// @@ usual ref-counted stuff here
+	ref_counted()
+		:
+		m_ref_count(0),
+		m_weak_proxy(0)
+	{
+	}
+
+	// @@ usual ref-counted stuff here for add_ref() and drop_ref()
 
 	virtual ~ref_counted()
 	{
 		assert(m_ref_count == 0);
-		if (m_weak_proxy) { m_weak_proxy->set_alive(false); }
+		if (m_weak_proxy)
+		{
+			m_weak_proxy->notify_object_died();
+			m_weak_proxy->drop_ref();
+			m_weak_proxy = NULL;
+		}
 	}
 
-	smart_ptr<weak_proxy> m_weak_proxy;
-	weak_proxy* get_weak_proxy()
+	weak_proxy* get_weak_proxy() const
 	{
 		if (m_weak_proxy == NULL)
 		{
 			m_weak_proxy = new weak_proxy;
+			m_weak_proxy->add_ref();
 		}
 	}
 };
