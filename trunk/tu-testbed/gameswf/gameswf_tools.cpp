9af876d19c65691673aa5f353f09994561ee2dc8
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2004-03-10 14:49:50 +0000

    stub for optional tools code

---
+++ trunk/tu-testbed/gameswf/gameswf_tools.cpp
@@ -0,0 +1,25 @@
+// gameswf_tools.cpp	-- Thatcher Ulrich <tu@tulrich.com> 2004
+
+// This source code has been donated to the Public Domain.  Do
+// whatever you want with it.
+
+// Some optional helper code.
+
+
+#include "base/tu_file.h"
+#include "base/utility.h"
+#include "gameswf.h"
+
+
+int	gameswf::tools::process_swf(tu_file* swf_in, tu_file* swf_out, const process_options& options)
+{
+}
+
+
+
+// Local Variables:
+// mode: C++
+// c-basic-offset: 8 
+// tab-width: 8
+// indent-tabs-mode: t
+// End:
