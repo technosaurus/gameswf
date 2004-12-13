e56f7b87dbf214dbfcb35e095bf4d06cd61b3a89
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2004-12-13 16:32:54 +0000

    draft triangulation code based on FIST
    compiles; doesn't work yet

---
+++ tu-testbed/base/triangulate_float.cpp
@@ -0,0 +1,61 @@
+// triangulate_float.cpp	-- Thatcher Ulrich 2004
+
+// This source code has been donated to the Public Domain.  Do
+// whatever you want with it.
+
+// Code to triangulate arbitrary 2D polygonal regions.
+//
+// Instantiate our templated algo from triangulate_inst.h
+
+#include "base/triangulate_impl.h"
+
+
+namespace triangulate
+{
+	// Version using float coords
+	void	compute(
+		array<float>* result,	// trilist
+		int path_count,
+		const array<float> **paths)
+	{
+		compute_triangulation<float>(result, path_count, paths);
+	}
+}
+
+
+
+#ifdef TEST_TRIANGULATE_FLOAT
+
+// Compile test with something like:
+//
+// gcc -o triangulate_test -I../ triangulate_float.cpp tu_random.cpp -DTEST_TRIANGULATE_FLOAT -lstdc++
+
+
+int	main()
+{
+	// test linkage.
+	array<float>	result;
+	array<array<float> >	paths;
+
+	paths.resize(1);
+	paths[0].push_back(0);
+	paths[0].push_back(0);
+	paths[0].push_back(1);
+	paths[0].push_back(0);
+	paths[0].push_back(1);
+	paths[0].push_back(1);
+	paths[0].push_back(0);
+	paths[0].push_back(1);
+
+	triangulate::compute(&result, 0, (const array<float>**) &(paths[0]));
+
+	for (int i = 0; i < result.size(); i++)
+	{
+		printf("%f\n", result[i]);
+	}
+
+	return 0;
+}
+
+
+#endif // TEST_TRIANGULATE_FLOAT
