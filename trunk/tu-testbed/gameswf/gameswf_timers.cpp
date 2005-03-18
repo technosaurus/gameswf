f57007a6dca6d63cf19d78639840889ffbc6dd46
rsavoye <rsavoye@4a52ceea-2c23-0410-a973-b1084b30d411>
2005-03-18 01:29:27 +0000

    Add Public Domain copyright notice for new files.

--- trunk/tu-testbed/gameswf/gameswf_timers.cpp
+++ trunk/tu-testbed/gameswf/gameswf_timers.cpp
@@ -1,3 +1,8 @@
+// gameswf_xml.h      -- Rob Savoye <rob@welcomehome.org> 2005
+
+// This source code has been donated to the Public Domain.  Do
+// whatever you want with it.
+
 #include "gameswf_log.h"
 #include "gameswf_action.h"
 #include "gameswf_impl.h"
