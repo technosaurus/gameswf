fdf36cc17decb27a240aedb62c6927b0deb8308c
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2005-05-09 15:02:50 +0000

    Some refactoring of ActionScript string code.

--- trunk/tu-testbed/gameswf/gameswf_string.h
+++ trunk/tu-testbed/gameswf/gameswf_string.h
@@ -3,70 +3,24 @@
 // This source code has been donated to the Public Domain. Do whatever
 // you want with it.
 
-#ifndef __STRING_H__
-#define __STRING_H__
+// Implementation for ActionScript String object.
 
-// tulrich: NO <iostream> ALLOWED IN GAMESWF
-//#include <iostream>
 
-#include "gameswf_log.h"
+#ifndef GAMESWF_STRING_H
+#define GAMESWF_STRING_H
+
+
 #include "gameswf_action.h"
-#include "gameswf_impl.h"
+
 
 namespace gameswf 
 {
-	// @@ tulrich: rename this!  Or, just put it in string_as_object.
-	class String
-	{
-	public:
-		String(const char* str);
-		String(const tu_string& str);
-		~String();
-		void test(gameswf::as_value* result, gameswf::as_object_interface* this_ptr, gameswf::as_environment* env, int nargs, int first_arg);
+	// Dispatcher for handling string methods.
+	void string_method(const fn_call& fn, const tu_stringi& method_name, const tu_string& this_string);
 
-		// tulrich: Most/all of these should take const references.
-		// Some should probably return void also?
-		tu_string charAt(short index);
-		short charCodeAt(short index);
-		tu_string concat(tu_string str1, tu_string str2);
-		tu_string fromCharCode(short code);
-		short indexOf(tu_string str, int short);
-		short indexOf(tu_string str);
-		tu_string lastIndexOf(tu_string, int index);
-		tu_string lastIndexOf(tu_string);
-		tu_string slice(int start);
-		tu_string slice(int start, int end);
-
-		tu_string split(tu_string str);
-		tu_string split(tu_string, int limit);
-		tu_string substr(int start);
-		tu_string substr(int start, int end);
-		tu_string substring(int start);
-		tu_string substring(int start, int end);
-		tu_string toLowerCase();
-		tu_string toUpperCase();
-
-		int length();
-	private:
-		tu_string m_value;
-	};
-
-	struct string_as_object : public gameswf::as_object
-	{
-          //String str;
-        };
-        
-	struct tu_string_as_object : public gameswf::as_object
-	{
-          tu_string str;
-	};
+	// Constructor for creating ActionScript String object.
+	void string_ctor(const fn_call& fn);
+}
 
 
-	void string_lastIndexOf(const fn_call& fn);
-	void string_new(const fn_call& fn);
- 	void string_from_char_code(const fn_call& fn);
-	void string_char_code_at(const fn_call& fn);
-
-} // end of gameswf namespace
-
-#endif // __STRING_H__
+#endif // GAMESWF_STRING_H
