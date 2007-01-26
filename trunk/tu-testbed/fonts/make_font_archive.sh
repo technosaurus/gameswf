42da3f9c54c0d12b6e78c2098ca85146c6d4f4a8
tulrich <tulrich@4a52ceea-2c23-0410-a973-b1084b30d411>
2007-01-26 10:45:40 +0000

    Add .zip output

--- trunk/tu-testbed/fonts/make_font_archive.sh
+++ trunk/tu-testbed/fonts/make_font_archive.sh
@@ -3,9 +3,16 @@
 # Package up font files w/ LICENSE.txt
 
 datestamp=`date +%Y%m%d`
-filename="tuffy-$datestamp.tar.gz"
+tar_filename="tuffy-$datestamp.tar.gz"
+zip_filename="tuffy-$datestamp.zip"
 
+files="LICENSE.txt tuffy_regular.sfd tuffy_bold.sfd tuffy_bold_italic.sfd tuffy_italic.sfd Tuffy.ttf Tuffy_Bold.ttf Tuffy_Bold_Italic.ttf Tuffy_Italic.ttf"
+
+# Make a tarball
+#
 # A sed pattern used by tar to prepend a path to each filename.
 transform_pattern=s:^:tuffy-$datestamp/:
+tar czvf $tar_filename --transform="$transform_pattern" $files
 
-tar czvf $filename LICENSE.txt --transform="$transform_pattern" tuffy_regular.sfd tuffy_bold.sfd tuffy_bold_italic.sfd tuffy_italic.sfd Tuffy.ttf Tuffy_Bold.ttf Tuffy_Bold_Italic.ttf Tuffy_Italic.ttf
+# Also make a .zip file.
+zip $zip_filename $files
