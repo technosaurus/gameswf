b8f3fae9898fb18be6406a4d1f92d1bcdfc9c11f
vitaly-a <vitaly-a@4a52ceea-2c23-0410-a973-b1084b30d411>
2007-05-01 17:07:57 +0000

    * gameswf/gameswf_3ds.*: moved to gameswf/plugins/lib3ds/ directory 	* gameswf/plugins/lib3ds/test.(fla,swf): added testcase 	* gameswf/plugins/lib3ds/cup.3ds: added 3D model that is used in test.swf

--- trunk/tu-testbed/gameswf/plugins/lib3ds/gameswf_3ds.h
+++ trunk/tu-testbed/gameswf/plugins/lib3ds/gameswf_3ds.h
@@ -3,14 +3,15 @@
 // This source code has been donated to the Public Domain.  Do
 // whatever you want with it.
 
-// lib3ds interface
+// Lib3ds plugin implementation for gameswf library
+// Now this is testbed, not release, please do not use in real game
 
 #ifndef GAMESWF_3DS_H
 #define GAMESWF_3DS_H
 
 #if TU_CONFIG_LINK_TO_LIB3DS == 1
 
-#include "gameswf_impl.h"
+#include "../../gameswf_impl.h"
 #include "base/tu_opengl_includes.h"
 
 #include <lib3ds/types.h>
@@ -25,7 +26,7 @@ namespace gameswf
 
 		virtual character* create_character_instance(character* parent, int id);
 		virtual void	display(character* inst);
-	
+
 		Lib3dsCamera* create_camera();
 		void remove_camera(Lib3dsCamera* camera);
 
@@ -35,6 +36,8 @@ namespace gameswf
 
 		Lib3dsVector m_bmin, m_bmax;
 		float	m_sx, m_sy, m_sz; // bounding box dimensions
+		float m_size;
+
 		float	m_cx, m_cy, m_cz; // bounding box center
 		int m_lightList;
 
@@ -51,12 +54,17 @@ namespace gameswf
 		virtual bool	get_member(const tu_stringi& name, as_value* val);
 		virtual bool	set_member(const tu_stringi& name, const as_value& val);
 
+		void	apply_transormation(float* target, float* camera_pos);
+
 		Lib3dsCamera* m_camera;
+		int m_texture_id;
+		float m_rotate[3];
 
 		private:
 		
 		smart_ptr<x3ds_definition>	m_def;
 		Lib3dsFloat m_current_frame;
+
 	};
 
 }	// end namespace gameswf
