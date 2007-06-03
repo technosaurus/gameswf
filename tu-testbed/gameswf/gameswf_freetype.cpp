040e864f9c57f5727b3b72ff79eb2cdaff938ebb
vitaly-a <vitaly-a@4a52ceea-2c23-0410-a973-b1084b30d411>
2007-06-03 11:29:08 +0000

    * gameswf/gameswf_freetype.cpp: added get_fontfile(...) function, 	this function gets font file name by font name (for Win32 only)

--- tu-testbed/gameswf/gameswf_freetype.cpp
+++ tu-testbed/gameswf/gameswf_freetype.cpp
@@ -10,12 +10,124 @@
 
 #include "gameswf/gameswf_render.h"
 #include "gameswf_freetype.h"
+#include "gameswf_log.h"
 #include "base/utility.h"
+#include "base/container.h"
+
+#ifdef _WIN32
+	#include <Windows.h>
+#else
+#endif
+
 
 #if TU_CONFIG_LINK_TO_FREETYPE == 1
 
 namespace gameswf
 {
+
+	bool get_fontfile(const char* font_name, tu_string& file_name)
+	// gets font file name by font name
+	{
+
+#ifdef _WIN32
+
+		//Vitaly: I'm not sure that this code works on all versions of Windows
+
+#define MAX_KEY_LENGTH 255
+#define MAX_VALUE_NAME 16383
+
+		HKEY hKey;
+
+		// try WinNT
+		DWORD retCode = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+			"Software\\Microsoft\\Windows NT\\CurrentVersion\\Fonts", 
+			0, 
+			KEY_ALL_ACCESS,
+			&hKey);
+
+		if (retCode != ERROR_SUCCESS)
+		{
+			// try Windows
+			retCode = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+				"Software\\Microsoft\\Windows\\CurrentVersion\\Fonts", 
+				0, 
+				KEY_ALL_ACCESS,
+				&hKey);
+
+			if (retCode != ERROR_SUCCESS)
+			{
+				return false;
+			}
+		}
+
+		// Get value count. 
+		DWORD    cValues;              // number of values for key 
+		retCode = RegQueryInfoKey(
+			hKey,	// key handle 
+			NULL,	// buffer for class name 
+			NULL,	// size of class string 
+			NULL,	// reserved 
+			NULL,	// number of subkeys 
+			NULL,	// longest subkey size 
+			NULL,	// longest class string 
+			&cValues,	// number of values for this key 
+			NULL,	// longest value name 
+			NULL,	// longest value data 
+			NULL,	// security descriptor 
+			NULL);	// last write time 
+
+
+		// Enumerate the key values. 
+		BYTE szValueData[MAX_PATH];
+		DWORD dwValueDataSize = sizeof(szValueData)-1;
+		TCHAR  achValue[MAX_VALUE_NAME]; 
+		DWORD cchValue = MAX_VALUE_NAME; 
+
+		tu_stringi fontname = tu_string(font_name) + " (TrueType)";
+		for (DWORD i = 0, retCode = ERROR_SUCCESS; i < cValues; i++) 
+		{ 
+			cchValue = MAX_VALUE_NAME; 
+			achValue[0] = '\0'; 
+			retCode = RegEnumValue(hKey, i, 
+				achValue, 
+				&cchValue, 
+				NULL, 
+				NULL,
+				szValueData,
+				&dwValueDataSize);
+
+			if (retCode == ERROR_SUCCESS) 
+			{ 
+				if (fontname == achValue)
+				{
+					// get bufsize required for windir environment variable
+					size_t requiredSize;
+					getenv_s( &requiredSize, NULL, 0, "WINDIR");
+
+					char* windir = (char*) malloc(requiredSize * sizeof(char));
+					assert(windir);
+
+					// Get the value of the 'windir' environment variable.
+					getenv_s(&requiredSize, windir, requiredSize, "WINDIR");
+					file_name = tu_string(windir) + tu_string("\\Fonts\\") + (char*) szValueData;
+
+					free(windir);
+
+					RegCloseKey(hKey);
+					return true;
+				}
+			} 
+		}
+
+		RegCloseKey(hKey);
+		return false;
+
+#else
+		log_error("get_fontfile() is not implemented yet\n");
+		return false;	//TODO for Linux
+#endif
+	}
+
 	// static
 	FT_Library	tu_freetype::m_lib;
 
@@ -45,36 +157,47 @@ namespace gameswf
 		return new tu_freetype(fontname);
 	}
 
-	tu_freetype::tu_freetype(const char* fontname)
+	tu_freetype::tu_freetype(const char* fontname) :
+		m_face(NULL)
 	{
 		if (m_lib == NULL)
 		{
 			init();
 		}
 
-		const char* font_filename = "/windows/fonts/times.ttf";	//vv fixme
-		int error = FT_New_Face(m_lib, font_filename, 0, &m_face);
-		if (error == FT_Err_Unknown_File_Format)
+		tu_string font_filename;
+		if (get_fontfile(fontname, font_filename) == false)
 		{
-			fprintf(stderr, "file '%s' has bad format\n", font_filename);
-			exit(1);
+			log_error("can't found font file for font '%s'\n", fontname);
+			return;
 		}
-		else
-			if (error)
-			{
-				fprintf(stderr, "some error opening font '%s'\n", font_filename);
-				exit(1);
-			}
 
-			// quality of image
-			error = FT_Set_Char_Size(
-				m_face,
-				0,	//width
-				12 * 64,	//height*64 in points!
-				300,	//horiz device dpi
-				300	//vert device dpi
-				);
-			assert(error == 0);
+		int error = FT_New_Face(m_lib, font_filename.c_str(), 0, &m_face);
+		switch (error)
+		{
+			case 0:
+				break;
+
+			case FT_Err_Unknown_File_Format:
+				log_error("file '%s' has bad format\n", font_filename.c_str());
+				return;
+				break;
+
+			default:
+				log_error("some error opening font '%s'\n", font_filename.c_str());
+				return;
+				break;
+		}
+
+		// quality of image
+		error = FT_Set_Char_Size(
+			m_face,
+			0,	//width
+			12 * 64,	//height*64 in points!
+			300,	//horiz device dpi
+			300	//vert device dpi
+			);
+		assert(error == 0);
 	}
 
 	tu_freetype::~tu_freetype()
