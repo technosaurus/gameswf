fa2078c4567aa217fe5b864047700653dbf2e9f2
vitaly-a <vitaly-a@4a52ceea-2c23-0410-a973-b1084b30d411>
2007-09-24 11:52:08 +0000

    added 'exported_module' modifier to some functions

--- tu-testbed/gameswf/gameswf_action.h
+++ tu-testbed/gameswf/gameswf_action.h
@@ -30,7 +30,7 @@ namespace gameswf
 	void clear_garbage();
 
 	// Dispatching methods from C++.
-	as_value	call_method(
+	exported_module as_value	call_method(
 		const as_value& method,	as_environment* env, as_object_interface* this_ptr,
 		int nargs, int first_arg_bottom_index);
 
