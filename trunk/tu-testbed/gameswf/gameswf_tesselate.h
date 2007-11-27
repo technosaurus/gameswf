b5a7258939a442fa7d6f370d2838ca8a2b84459b
vitaly-a <vitaly-a@4a52ceea-2c23-0410-a973-b1084b30d411>
2007-11-27 07:59:09 +0000

    tabs

--- trunk/tu-testbed/gameswf/gameswf_tesselate.h
+++ trunk/tu-testbed/gameswf/gameswf_tesselate.h
@@ -19,39 +19,39 @@ namespace gameswf
 {
 	namespace tesselate
 	{
-struct trapezoid
-{
-	float	m_y0, m_y1;
-	float	m_lx0, m_lx1, m_rx0, m_rx1;
-};
+		struct trapezoid
+		{
+			float	m_y0, m_y1;
+			float	m_lx0, m_lx1, m_rx0, m_rx1;
+		};
 
-// Interface for receiving the results of
-// trapezoidation.
-struct trapezoid_accepter
-{
-	virtual ~trapezoid_accepter() {}
-	virtual void	accept_trapezoid(int style, const trapezoid& tr) = 0;
-	virtual void	accept_line_strip(int style, const point coords[], int coord_count) = 0;
-};
+		// Interface for receiving the results of
+		// trapezoidation.
+		struct trapezoid_accepter
+		{
+			virtual ~trapezoid_accepter() {}
+			virtual void	accept_trapezoid(int style, const trapezoid& tr) = 0;
+			virtual void	accept_line_strip(int style, const point coords[], int coord_count) = 0;
+		};
 
-// A shape has one or more paths.  The paths in a
-// shape are tesselated together using a typical
-// polygon odd-even rule.
-//
-// The error tolerance tells the tesselator how much
-// geometric error is allowed along curve edges.
-void	begin_shape(trapezoid_accepter* accepter, float curve_error_tolerance);
-void	end_shape();
+		// A shape has one or more paths.  The paths in a
+		// shape are tesselated together using a typical
+		// polygon odd-even rule.
+		//
+		// The error tolerance tells the tesselator how much
+		// geometric error is allowed along curve edges.
+		void	begin_shape(trapezoid_accepter* accepter, float curve_error_tolerance);
+		void	end_shape();
 
-// A path is enclosed within a shape.  If fill styles
-// are active, a path should be a closed shape
-// (i.e. the last point should match the first point).
-// Set your styles before rendering the path; all
-// segments in a path must have the same styles.
-void	begin_path(int style_left, int style_right, int line_style, float ax, float ay);
-void	add_line_segment(float ax, float ay);
-void	add_curve_segment(float cx, float cy, float ax, float ay);
-void	end_path();
+		// A path is enclosed within a shape.  If fill styles
+		// are active, a path should be a closed shape
+		// (i.e. the last point should match the first point).
+		// Set your styles before rendering the path; all
+		// segments in a path must have the same styles.
+		void	begin_path(int style_left, int style_right, int line_style, float ax, float ay);
+		void	add_line_segment(float ax, float ay);
+		void	add_curve_segment(float cx, float cy, float ax, float ay);
+		void	end_path();
 
 	};	// end namespace tesselate
 
