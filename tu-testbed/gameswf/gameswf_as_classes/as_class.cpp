08ab8d90763d8cb5f6987c06472d3cdd8b028cfb
Crazyjul <Crazyjul@4a52ceea-2c23-0410-a973-b1084b30d411>
2008-07-04 16:58:31 +0000

    First implementation in avm2 construction. Still need to apply traits and create prototype chain

---
+++ tu-testbed/gameswf/gameswf_as_classes/as_class.cpp
@@ -0,0 +1,8 @@
+// as_class.cpp	-- Julien Hamaide <julien.hamaide@gmail.com> 2008
+
+// This source code has been donated to the Public Domain.  Do
+// whatever you want with it.
+
+// Action Script 3 Class object
+
+#include "as_class.h"
