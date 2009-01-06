a1861b4b277c63f6e958ba2eaebaa352d49355ac
vitaly-a <vitaly-a@4a52ceea-2c23-0410-a973-b1084b30d411>
2009-01-06 08:51:29 +0000

    cleanup: - removed experimental is_visible() stuff - as_netstream more readable

--- trunk/tu-testbed/gameswf/gameswf_text.cpp
+++ trunk/tu-testbed/gameswf/gameswf_text.cpp
@@ -310,12 +310,6 @@ namespace gameswf
 	void	text_character_def::display(character* inst)
 	// Draw the string.
 	{
-		if (inst->is_visible() == false)
-		{
-			// We're invisible, so don't display!
-			return;
-		}
-
 		display_glyph_records(m_matrix, inst, m_text_glyph_records, m_root_def);
 	}
 
@@ -733,12 +727,6 @@ namespace gameswf
 
 	void edit_text_character::display() 
 	{ 
-		if (is_visible() == false)
-		{
-			// We're invisible, so don't display!
-			return;
-		}
-
 		if (m_def->m_border == true) 
 		{ 
 			matrix  mat = get_world_matrix(); 
