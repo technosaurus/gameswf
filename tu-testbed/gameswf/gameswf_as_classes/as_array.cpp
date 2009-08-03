b725f0045498011080db01e6ee11f0a47bd8a226
vitaly-a <vitaly-a@4a52ceea-2c23-0410-a973-b1084b30d411>
2009-08-03 10:43:46 +0000

    rewritten as_array, used array<as_value> container

--- tu-testbed/gameswf/gameswf_as_classes/as_array.cpp
+++ tu-testbed/gameswf/gameswf_as_classes/as_array.cpp
@@ -10,31 +10,25 @@
 namespace gameswf
 {
 
+	// public join([delimiter:String]) : String
+	// delimiter:String [optional] - A character or string that separates array elements in the returned string.
+	// If you omit this parameter, a comma (,) is used as the default separator.
 	void	as_array_join(const fn_call& fn)
 	{
 		as_array* a = cast_to<as_array>(fn.this_ptr);
-
-		if(a && fn.nargs > 0)
+		const char* delimiter = fn.nargs > 0 ? fn.arg(0).to_string() : ",";
+		if (a)
 		{
 			tu_string result;
-			const char * separator = fn.arg(0).to_string();
-
-			int size = a->size();
-			for(int index = 0; index < size; ++index )
+			for (int index = 0; index < a->size(); ++index)
 			{
-				as_value _index( index );
-				as_value value;
-				if( index != 0 ) result += separator;
-				a->get_member( _index.to_tu_stringi(), &value );
-
-				result += value.to_tu_string();
+				result += a->m_array[index].to_tu_string();
+				if (index < a->size() - 1)
+				{
+					result += delimiter;
+				}
 			}
-
-			fn.result->set_tu_string( result );
-		}
-		else
-		{
-			fn.result->set_undefined();
+			fn.result->set_tu_string(result);
 		}
 	}
 
@@ -47,30 +41,42 @@ namespace gameswf
 		}
 	}
 
+	// Adds one or more elements to the end of an array and returns the new length of the array.
 	void	as_array_push(const fn_call& fn)
 	{
 		as_array* a = cast_to<as_array>(fn.this_ptr);
 		if (a)
 		{
-			if (fn.nargs > 0)
+			for (int i = 0; i < fn.nargs; i++)
 			{
-				a->push(fn.arg(0));
+				a->push(fn.arg(i));
 			}
 			fn.result->set_int(a->size());
 		}
 	}
 
-	// remove the first item of array
+	// remove the first item of array and returns the value of that element.
 	void	as_array_shift(const fn_call& fn)
 	{
 		as_array* a = cast_to<as_array>(fn.this_ptr);
-		if (a)
+		if (a && a->size() > 0)
 		{
-			as_value val;
-			a->remove(0, &val);
-			*fn.result = val;
+			*fn.result = a->m_array[0];
+			a->remove(0);
 		}
 	}
+
+	//Removes the last element from an array and returns the value of that element.
+	void	as_array_pop(const fn_call& fn)
+	{
+		as_array* a = cast_to<as_array>(fn.this_ptr);
+		if (a && a->size() > 0)
+		{
+			*fn.result = a->m_array[a->size() - 1];
+			a->resize(a->size() - 1);
+		}
+	}
+
 
 	// public splice(startIndex:Number, [deleteCount:Number], [value:Object]) : Array
 	// adds elements to and removes elements from an array.
@@ -80,47 +86,48 @@ namespace gameswf
 		as_array* a = cast_to<as_array>(fn.this_ptr);
 		if (a && fn.nargs >= 1)
 		{
-			int index = fn.arg(0).to_int();
-			int asize = a->size();	// for optimization
-			if (index >=0 && index < asize)
+			// startIndex:Number - An integer that specifies the index of the element in the array
+			// where the insertion or deletion begins.
+			// You can specify a negative integer to specify a position relative to the end of the array 
+			// (for example, -1 is the last element of the array).
+			int index = fn.arg(0).to_int() >= 0 ? fn.arg(0).to_int() : a->size() + fn.arg(0).to_int();
+			if (index >= 0 && index < a->size())
 			{
-
-				// delete items
-
+				// Returns an array containing the elements that were removed from the original array.
 				as_array* deleted_items = new as_array(a->get_player());
 				fn.result->set_as_object(deleted_items);
 
-				int delete_count = asize - index;
-				if (fn.nargs >= 2)
-				{
-					delete_count = fn.arg(1).to_int();
-					if (delete_count > asize - index)
-					{
-						delete_count = asize - index;
-					}
-				}
+				// first delete items
 
+				// If no value is specified for the deleteCount parameter,
+				// the method deletes all of the values from the startIndex element to the last element 
+				int delete_count = a->size() - index;
+
+				if (fn.nargs >= 2 && fn.arg(1).to_int() < delete_count && fn.arg(1).to_int() >= 0)
+				{
+					// If the value is 0, no elements are deleted.
+					delete_count = fn.arg(1).to_int();
+				}
 				for (int i = 0; i < delete_count; i++)
 				{
-					as_value val;
-					a->remove(index, &val);
-					deleted_items->push(val);
+					deleted_items->push(a->m_array[index]);
+					a->remove(index);
 				}
 
-				// insert values
+				// then insert items
 
 				if (fn.nargs >= 3)
 				{
+					// Specifies the values to insert into the array at the insertion point specified in the startIndex parameter.
 					const as_value& val = fn.arg(2);
+
 					as_array* obj = cast_to<as_array>(val.to_object());
 					if (obj)
 					{
 						// insert an array
 						for (int i = obj->size() - 1; i >= 0; i--)
 						{
-							as_value val;
-							obj->get_member(tu_string(i), &val);
-							a->insert(index, val);
+							a->insert(index, obj->m_array[i]);
 						}
 					}
 					else
@@ -138,26 +145,31 @@ namespace gameswf
 	// in an array and creates a new array. 
 	// If the value parameters specify an array, the elements of that array are concatenated,
 	// rather than the array itself. The array my_array is left unchanged.
+	// If you don't pass any values, a duplicate of an array is created.
 	void	as_array_concat(const fn_call& fn)
 	{
 		as_array* a = cast_to<as_array>(fn.this_ptr);
-		if (a && fn.nargs > 0)
+		if (a)
 		{
 			as_array* res = new as_array(a->get_player());
 			fn.result->set_as_object(res);
 
-			a->copy_to(res);
+			// duplicate an array
+			res->resize(a->size());
+			for (int i = 0; i < a->size(); i++)
+			{
+				res->m_array[i] = a->m_array[i];
+			}
 
 			for (int i = 0; i < fn.nargs; i++)
 			{
 				as_array* arg = cast_to<as_array>(fn.arg(i).to_object());
 				if (arg)
 				{
+					// concat an array
 					for (int j = 0, n = arg->size(); j < n; j++)
 					{
-						as_value val;
-						arg->get_member(tu_string(j), &val);
-						res->push(val);
+						res->push(arg->m_array[j]);
 					}
 				}
 				else
@@ -172,6 +184,7 @@ namespace gameswf
 	// Returns a new array that consists of a range of elements from the original array,
 	// without modifying the original array. The returned array includes the startIndex 
 	// element and all elements up to, but not including, the endIndex element. 
+	// If you don't pass any parameters, a duplicate of the original array is created.
 	void	as_array_slice(const fn_call& fn)
 	{
 		as_array* a = cast_to<as_array>(fn.this_ptr);
@@ -183,35 +196,30 @@ namespace gameswf
 			// If you don't pass any parameters, a duplicate of the original array is created.
 			if (fn.nargs == 0)
 			{
-				a->copy_to(res);
+				res->resize(a->size());
+				for (int i = 0; i < a->size(); i++)
+				{
+					res->m_array[i] = a->m_array[i];
+				}
 				return;
 			}
 
-			// If start is a negative number, the starting point begins at the end of the array,
-			// where -1 is the last element.
-			int start = fn.arg(0).to_int();
-			if (start < 0)
-			{
-				start += a->size();
-			}
+			// start index
+			int start = fn.arg(0).to_int() >= 0 ? fn.arg(0).to_int() : a->size() + fn.arg(0).to_int();
 
 			// If you omit this parameter, the slice includes all elements
 			int end = a->size();
 			if (fn.nargs >= 2)
 			{
-				end = fn.arg(1).to_int();
-				if (end < 0)
-				{
-					end += a->size();
-				}
+				end = fn.arg(1).to_int() >= 0 ? fn.arg(1).to_int() : a->size() + fn.arg(1).to_int();
+				end = imin(end, a->size());
 			}
 
-			for (int i = start; i < end; i++)
+			if (start >= 0 && start < a->size())
 			{
-				as_value val;
-				if (a->get_member(tu_string(i), &val))
+				for (int i = start; i < end; i++)
 				{
-					res->push(val);
+					res->push(a->m_array[i]);
 				}
 			}
 		}
@@ -284,17 +292,6 @@ namespace gameswf
 		}
 	}
 
-	void	as_array_pop(const fn_call& fn)
-	{
-		as_array* a = cast_to<as_array>(fn.this_ptr);
-		if (a)
-		{
-			as_value val;
-			a->pop(&val);
-			*fn.result = val;
-		}
-	}
-
 	void	as_array_length(const fn_call& fn)
 	{
 		as_array* a = cast_to<as_array>(fn.this_ptr);
@@ -316,11 +313,10 @@ namespace gameswf
 
 			// Use the arguments as initializers.
 			int	size = fn.env->pop().to_int();
-			as_value	index_number;
+			ao->resize(size);
 			for (int i = 0; i < size; i++)
 			{
-				index_number.set_int(i);
-				ao->set_member(index_number.to_string(), fn.env->pop());
+				ao->m_array[i] = fn.env->pop();
 			}
 		}
 		else
@@ -330,12 +326,7 @@ namespace gameswf
 		{
 			// Create an empty array with the given number of undefined elements.
 			int size = fn.arg(0).to_int();
-			as_value	index_number;
-			for (int i = 0; i < size; i++)
-			{
-				index_number.set_int(i);
-				ao->set_member(index_number.to_string(), as_value());
-			}
+			ao->resize(size);
 		}
 		else
 
@@ -344,11 +335,10 @@ namespace gameswf
 			assert(fn.env);
 
 			// Use the arguments as initializers.
-			as_value	index_number;
+			ao->resize(fn.nargs);
 			for (int i = 0; i < fn.nargs; i++)
 			{
-				index_number.set_int(i);
-				ao->set_member(index_number.to_string(), fn.arg(i));
+				ao->m_array[i] = fn.arg(i);
 			}
 		}
 
@@ -387,215 +377,16 @@ namespace gameswf
 
 	const char* as_array::to_string()
 	{
-		// receive indexes
-		// array may contain not numerical indexes
-		array<tu_stringi> idx;
-		for (stringi_hash<as_value>::iterator it = m_members.begin(); it != m_members.end(); ++it)
-		{
-			if (it->second.is_enum())
-			{
-				idx.push_back(it->first);
-			}
-		}
-
-		// sort indexes
-		int n = idx.size();
-		for (int i = 0; i < n - 1; i++)
-		{
-			for (int j = i + 1; j < n; j++)
-			{
-				if (idx[i] > idx[j])
-				{
-					tu_swap(&idx[i], &idx[j]);
-				}
-			}
-		}
-
-		// form string
 		m_string_value = "";
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < size(); i++)
 		{
-			as_value val;
-			as_object::get_member(idx[i], &val);
-			m_string_value += val.to_string();
-
-			if (i < n - 1)
+			m_string_value += m_array[i].to_tu_string();
+			if (i < size() - 1)
 			{
 				m_string_value +=  ",";
 			}
 		}
-
 		return m_string_value.c_str();
-	}
-
-	int as_array::size()
-	// get array size, excluding own methods
-	{
-		int n = 0;
-		for (stringi_hash<as_value>::iterator it = m_members.begin(); it != m_members.end(); ++it)
-		{
-			if (it->second.is_enum())
-			{
-				n++;
-			}
-		}
-		return n;
-	}
-
-	void as_array::push(const as_value& val)
-	// Insert the given value at the end of the array.
-	{
-		as_value index(size());
-
-		as_value enum_val(val);
-		enum_val.set_flags(0);
-
-		set_member(index.to_tu_stringi(), enum_val);
-	}
-
-	void as_array::remove(int index, as_value* val)
-	// Removes the element starting from 'index' and returns the value of that element.
-	{
-		assert(val);
-
-		// receive numerical indexes
-		array<int> idx;
-		for (stringi_hash<as_value>::iterator it = m_members.begin(); it != m_members.end(); ++it)
-		{
-			int index;
-			if (string_to_number(&index, it->first.c_str()) && it->second.is_enum())
-			{
-				idx.push_back(index);
-			}
-		}
-
-		if (index >= 0 && index < idx.size())
-		{
-			// sort indexes
-			for (int i = 0, n = idx.size(); i < n - 1; i++)
-			{
-				for (int j = i + 1; j < n; j++)
-				{
-					if (idx[i] > idx[j])
-					{
-						tu_swap(&idx[i], &idx[j]);
-					}
-				}
-			}
-
-			// keep the element starting from 'index'
-			get_member(tu_string(idx[index]), val);
-
-			// shift elements
-			for (int i = index + 1; i < idx.size(); i++)
-			{
-				as_value v;
-				get_member(tu_string(idx[i]), &v);
-				set_member(tu_string(idx[i - 1]), v);
-			}
-
-			// remove the last element
-			erase(tu_string(idx[idx.size() - 1]));
-		}
-		else
-		{
-			//assert(0);
-		}
-	}
-
-
-	void as_array::insert(int index, const as_value& val)
-	// Inserts the element after 'index'
-	{
-		// receive numerical indexes
-		array<int> idx;
-		for (stringi_hash<as_value>::iterator it = m_members.begin(); it != m_members.end(); ++it)
-		{
-			int index;
-			if (string_to_number(&index, it->first.c_str()) && it->second.is_enum())
-			{
-				idx.push_back(index);
-			}
-		}
-
-		if (index >= 0 && index < idx.size())
-		{
-			// add new index
-			idx.push_back(idx.size());
-
-			// sort indexes
-			for (int i = 0, n = idx.size(); i < n - 1; i++)
-			{
-				for (int j = i + 1; j < n; j++)
-				{
-					if (idx[i] > idx[j])
-					{
-						tu_swap(&idx[i], &idx[j]);
-					}
-				}
-			}
-
-			// shift elements
-			for (int i = idx.size() - 1; i > index; i--)
-			{
-				as_value v;
-				get_member(tu_string(idx[i - 1]), &v);
-				set_member(tu_string(idx[i]), v);
-			}
-
-			// insert
-			set_member(tu_string(idx[index]), val);
-
-		}
-		else
-		{
-			//assert(0);
-		}
-	}
-
-
-
-	void as_array::pop(as_value* val)
-	// Removes the last element from an array and returns the value of that element.
-	{
-		assert(val);
-		as_value index(size() - 1);
-		if (get_member(index.to_tu_stringi(), val))
-		{
-			erase(index.to_tu_stringi());
-		}
-	}
-
-	void as_array::erase(const tu_stringi& index)
-	// erases one member from array
-	{
-		m_members.erase(index);
-	}
-
-	void as_array::clear()
-	// erases all members from array
-	{
-		for (int i = 0, n = size(); i < n; i++)
-		{
-			as_value index(i);
-			m_members.erase(index.to_tu_stringi());
-		}
-	}
-
-	void as_array::copy_to(as_object* target)
-	// Copy items from 'this' to target
-	{
-		if (target)
-		{
-			for (stringi_hash<as_value>::const_iterator it = m_members.begin(); 
-				it != m_members.end(); ++it ) 
-			{ 
-				if (it->second.is_enum())
-				{
-					target->set_member(it->first, it->second); 
-				}
-			} 
-		}
 	}
 
 	// By default, Array.sort() works as described in the following list:
@@ -604,20 +395,14 @@ namespace gameswf
 	// Numeric fields are sorted as if they were strings, so 100 precedes 99
 	void as_array::sort(int options, as_function* compare_function)
 	{
-		int n = size();
-		for (int i = 0; i < n - 1; i++)
+		for (int i = 0; i < size() - 1; i++)
 		{
-			as_value ival;
-			get_member(tu_string(i), &ival);
-			for (int j = i + 1; j < n; j++)
+			for (int j = i + 1; j < size(); j++)
 			{
-				as_value jval;
-				get_member(tu_string(j), &jval);
-
 				bool do_swap = false;
 				if (compare_function == NULL)
 				{
-					if (ival.to_tu_string() > jval.to_tu_string())
+					if (m_array[i].to_tu_string() > m_array[j].to_tu_string())
 					{
 						do_swap = true;
 					}
@@ -635,8 +420,8 @@ namespace gameswf
 					// keep stack size
 					int stack_size = env->get_stack_size();
 
-					env->push(jval);
-					env->push(ival);
+					env->push(m_array[j]);
+					env->push(m_array[i]);
 					as_value ret = call_method(compare_function, env, this, 2, env->get_top_index());
 
 					// restore stack size
@@ -655,14 +440,41 @@ namespace gameswf
 
 				if (do_swap)
 				{
-					as_value tmp(ival);
-					ival = jval;
-					jval = tmp;
-					set_member(tu_string(i), ival);
-					set_member(tu_string(j), jval);
+					tu_swap(&m_array[i], &m_array[j]);
 				}
 			}
 		}
 	}
 
+	bool	as_array::get_member(const tu_stringi& name, as_value* val)
+	{
+		int index;
+		if (string_to_number(&index, name.c_str(), 10))
+		{
+			if (index >= 0 && index < size())
+			{
+				*val = m_array[index];
+				return true;
+			}
+		}
+		return as_object::get_member(name, val);
+	}
+
+	bool	as_array::set_member(const tu_stringi& name, const as_value& val)
+	{
+		int index;
+		if (string_to_number(&index, name.c_str(), 10))
+		{
+			if (index >= 0)
+			{
+				if (index >= size())
+				{
+					resize(index + 1);
+				}
+				m_array[index] = val;
+				return true;
+			}
+		}
+		return as_object::set_member(name, val);
+	}
 };
